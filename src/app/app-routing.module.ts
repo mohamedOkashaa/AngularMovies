ab11228d84b150b7856389c14355b79db2a45b26
mohamed okasha <105164560+mohamedOkashaa@users.noreply.github.com>
2023-08-20 17:25:54 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,15 +14,15 @@ import { TvComponent } from './tv/tv.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home',canActivate:[AuthGuard],component:HomeComponent,title:'Home'},
-  {path:'movies/:page',canActivate:[AuthGuard],component:MoviesComponent,title:'Movies'},
-  {path:'tv/:page',canActivate:[AuthGuard],component:TvComponent,title:'Tv Show'},
-  {path:'person/:page',canActivate:[AuthGuard],component:PeopleComponent,title:'People'},
-  {path:'details/:id/:type',canActivate:[AuthGuard],component:DetailsComponent,title:'Details'},
-  {path:'about',canActivate:[AuthGuard],component:AboutComponent,title:'About'},
-  {path:'search',canActivate:[AuthGuard],component:SearchComponent,title:'Search'},
-  {path:'login',canActivate:[AuthGuardLogin],component:LoginComponent,title:'LogIn'},
-  {path:'signup',canActivate:[AuthGuardLogin],component:SignupComponent,title:'SignUp'},
+  {path:'home', /* canActivate:[AuthGuard] , */ component:HomeComponent,title:'Home'},
+  {path:'movies/:page',/* canActivate:[AuthGuard] , */component:MoviesComponent,title:'Movies'},
+  {path:'tv/:page',/* canActivate:[AuthGuard] , */component:TvComponent,title:'Tv Show'},
+  {path:'person/:page',/* canActivate:[AuthGuard] , */component:PeopleComponent,title:'People'},
+  {path:'details/:id/:type',/* canActivate:[AuthGuard] , */component:DetailsComponent,title:'Details'},
+  {path:'about',/* canActivate:[AuthGuard] , */component:AboutComponent,title:'About'},
+  {path:'search',/* canActivate:[AuthGuard] , */component:SearchComponent,title:'Search'},
+  {path:'login',/* canActivate:[AuthGuardLogin] */ component:LoginComponent,title:'LogIn'},
+  {path:'signup' , /* canActivate:[AuthGuardLogin],  */ component:SignupComponent,title:'SignUp'},
   {path:'**',component:NotfoundComponent,title:'NotFound'}
 ];
 
