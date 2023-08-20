64161149c22afac79877e6e788810e21d9836dc6
mohamed okasha <105164560+mohamedOkashaa@users.noreply.github.com>
2023-08-20 17:55:56 +0300

    Delete src directory

--- src/app/progrees.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProgreesPipe } from './progrees.pipe';
-
-describe('ProgreesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ProgreesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
